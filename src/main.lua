f1c96e677f9fe158a2f56faa6b7a56627efa36b0
bullno1 <thebullno1@gmail.com>
2014-06-15 23:20:09 +0800

    Move all plugin code to plugins folder

--- src/main.lua
+++ src/main.lua
@@ -17,11 +17,15 @@ table.insert(package.loaders, javaLoader)
 
 -- Make a toast for luajava
 Toast = require "android.widget.Toast" -- just require it
-MoaiActivity = require "ch.levietba.sample.MoaiActivity"
+MoaiActivity = require "ch.levietba.moaijni.MoaiActivity"
 local appCtx = MoaiActivity:getCurrentInstance():getApplicationContext()
 local duration = Toast.LENGTH_LONG
 text = Toast:makeText(appCtx, "Hello world from JNI", duration)
 text:show()
+
+-- Do things we couldn't trivially do before
+Environment = require "android.os.Environment"
+print("SD card path:", Environment:getExternalStorageDirectory():toString())
 
 -- Let's crash
 require "somethingsomething"
